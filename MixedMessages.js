ad05dbe4b1145dbe6d99cc674f98846195cd5a7c
kanakana19 <ionut_paslariu88@yahoo.com>
2021-02-14 20:02:21 +0200

    The arrays that we will work on.

---
+++ MixedMessages.js
@@ -0,0 +1,4 @@
+const sentenceArr = [['Every time I ask someone about you, I hear these 3 words: '], ['You should stop being so: '], ['20 years ago people were more:  '], ['You always expect people to tell you that you are: ']]
+const firstQual = [['good'], ['honest'], ['friendly'], ['loyal']]
+const secondQual = [['ambitious'], ['lazy'], ['dishonest'], ['fair']]
+const thirdQual = [['sneaky'], ['gregarious'], ['forgiving'], ['brave']]
