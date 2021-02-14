31181ac8c9b8f0a66f843b81399981072fa2f456
kanakana19 <ionut_paslariu88@yahoo.com>
2021-02-14 20:29:21 +0200

    End of project

--- MixedMessages.js
+++ MixedMessages.js
@@ -2,3 +2,12 @@ const sentenceArr = [['Every time I ask someone about you, I hear these 3 words:
 const firstQual = [['good'], ['honest'], ['friendly'], ['loyal']]
 const secondQual = [['ambitious'], ['lazy'], ['dishonest'], ['fair']]
 const thirdQual = [['sneaky'], ['gregarious'], ['forgiving'], ['brave']]
+
+
+let a = sentenceArr[Math.floor(Math.random()*4)]
+let b = firstQual[Math.floor(Math.random()*4)]
+let c = secondQual[Math.floor(Math.random()*4)]
+let d = thirdQual[Math.floor(Math.random()*4)]
+let theMessage = a + b + ' ' + c + ' and ' + d
+console.log(theMessage);
+
